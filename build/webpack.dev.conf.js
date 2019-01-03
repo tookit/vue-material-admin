feccde2abdfc2f0970279ab068d10779b7573f65
tookit <wangqiangshen@gmail.com>
2019-01-03 11:21:12 +0800

    update webpack config

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -20,6 +20,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
 
+  mode: 'development',
   // these devServer options should be customized in /config/index.js
   devServer: {
     clientLogLevel: 'warning',
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,6 +24,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     })
   },
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
+  mode: 'production',
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
