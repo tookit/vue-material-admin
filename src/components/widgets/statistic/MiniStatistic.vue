1fc130a71694180a0bfeea85106516b3ecc61b3d
Michael Wang <wangqiangshen@gmail.com>
2020-07-31 19:37:35 +0800

    update mini card

--- src/components/widgets/statistic/MiniStatistic.vue
+++ src/components/widgets/statistic/MiniStatistic.vue
@@ -1,22 +1,17 @@
 <template>
-  <v-card>
+  <v-card tile>
     <v-card-text class="pa-0">
-      <v-container class="pa-0">
-        <div class="layout row ma-0">
-          <div class="sm6 xs6 flex">
-            <div class="layout column ma-0 justify-center align-center">
-              <v-icon size="56px" :color="color">{{ icon }}</v-icon>
-            </div>
+      <v-row>
+        <v-col cols="6">
+          <div class="layout column ma-0 justify-center align-center">
+            <v-icon size="56px" :color="color">{{ icon }}</v-icon>
           </div>
-          <div
-            class="sm6 xs6 flex text-sm-center py-3 white--text"
-            :class="color"
-          >
-            <div class="headline">{{ title }}</div>
-            <span class="caption">{{ subTitle }}</span>
-          </div>
-        </div>
-      </v-container>
+        </v-col>
+        <v-col cols="6" :class="color">
+          <div class="white--text">{{ title }}</div>
+          <span class="white--text">{{ subTitle }}</span>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
@@ -27,7 +22,7 @@ export default {
     icon: String,
     title: String,
     subTitle: String,
-    color: String,
-  },
+    color: String
+  }
 }
 </script>
