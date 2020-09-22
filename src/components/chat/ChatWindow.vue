9440e26e171c61a9af4d6bb648c017289e84f614
michaelwang <wangqiangshen@gmail.com>
2020-09-22 09:02:50 +0800

    fixed chat window

--- src/components/chat/ChatWindow.vue
+++ src/components/chat/ChatWindow.vue
@@ -2,23 +2,22 @@
   <v-card class="chat-room">
     <v-app-bar text dense class="white chat-room--toolbar" light>
       <v-btn icon>
-        <v-icon color="text--secondary">keyboard_arrow_left</v-icon>
+        <v-icon color="text--secondary">mdi-arrow-left</v-icon>
       </v-btn>
       <template v-if="chat.users">
-        <v-avatar
+        <c-avatar
           size="32"
           class="avatar-stack"
           v-for="(user_id, index) in chat.users"
+          :src="getAvatar(user_id)"
           :key="index"
-        >
-          <img :src="getAvatar(user_id)" alt="" />
-        </v-avatar>
+        />
       </template>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-toolbar-title>
         <h4>Chat Channel</h4>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">
           <v-btn icon slot="activator" v-on="on">
@@ -35,14 +34,21 @@
       <v-card-text class="chat-room--list pa-3">
         <template v-for="(item, index) in chat.messages">
           <div
-            :class="[index % 2 == 0 ? 'reverse' : '']"
-            class="messaging-item layout row my-4"
+            :class="[index % 2 == 0 ? 'flex-row-reverse' : '']"
+            class="messaging-item"
             :key="index"
           >
-            <v-avatar class="indigo mx-1" size="40">
-              <img :src="item.user.avatar" alt="" />
-            </v-avatar>
-            <div class="messaging--body layout column mx-2">
+            <div class="messaging-item__avatar">
+              <c-avatar
+                :size="36"
+                :src="item.user.avatar"
+                :username="item.user.username"
+                status="online"
+                online
+                :key="index"
+              />
+            </div>
+            <div class="messaging-item__body">
               <p
                 :value="true"
                 :class="[index % 2 == 0 ? 'primary white--text' : 'white']"
@@ -80,46 +86,56 @@
 import { getChatById } from '@/api/chat'
 import { getUserById } from '@/api/user'
 import VuePerfectScrollbar from 'vue-perfect-scrollbar'
+import CAvatar from '@/components/avatar/CAvatar'
 export default {
   components: {
     VuePerfectScrollbar,
+    CAvatar
   },
   props: {
     uuid: {
       type: String,
-      default: '',
+      default: ''
     },
     height: {
       type: String,
-      default: null,
-    },
+      default: null
+    }
   },
   computed: {
     chat() {
       let chatOrigin = {
         title: 'Chat',
         users: [],
-        messages: [],
+        messages: []
       }
       let chat = getChatById(this.$route.params.uuid)
       return Object.assign(chatOrigin, chat)
     },
     computeHeight() {
       return {
-        height: this.height || '',
+        height: this.height || ''
       }
-    },
+    }
   },
 
   methods: {
     getAvatar(uid) {
       return getUserById(uid).avatar
-    },
-  },
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
-.chat-room--scrollbar
-  height: calc(100vh - 48px - 56px)
+<style lang="scss" scoped>
+.chat-room--scrollbar {
+  height: calc(100vh - 48px - 56px);
+  .messaging-item {
+    display: flex;
+    margin-bottom: 20px;
+    &__avatar {
+      margin-right: 15px;
+    }
+  }
+}
 </style>
