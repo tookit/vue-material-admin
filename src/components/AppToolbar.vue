1eaf16dcef8b4635d47fe546e58a64ab520982af
michaelwang <wangqiangshen@gmail.com>
2020-09-25 12:13:28 +0800

    add blog link

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -4,6 +4,7 @@
     <v-spacer />
     <v-toolbar-items>
       <v-btn text href="mailto:wangqiangshen@gmail.com">Hire Me</v-btn>
+      <v-btn text href="https://www.isocked.com" target="_blank">Blog</v-btn>
       <v-btn icon href="https://github.com/tookit/vue-material-admin">
         <v-icon>mdi-github</v-icon>
       </v-btn>
