1ea41b166f3a8dda357983bdc23d7de107eeffce
michaelwang <wangqiangshen@gmail.com>
2020-09-25 12:23:59 +0800

    update changelog

--- src/views/Changelog.vue
+++ src/views/Changelog.vue
@@ -1,8 +1,43 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
-        changelog
+      <v-col :cols="12">
+        <v-card tile>
+          <v-card-title>Changelog</v-card-title>
+          <v-divider />
+          <v-card-text>
+            <v-timeline>
+              <v-timeline-item
+                v-for="(year, i) in years"
+                :key="i"
+                :color="year.color"
+                small
+              >
+                <template v-slot:opposite>
+                  <span
+                    :class="`headline font-weight-bold ${year.color}--text`"
+                    v-text="year.year"
+                  ></span>
+                </template>
+                <div class="py-4">
+                  <h2
+                    :class="
+                      `headline font-weight-light mb-4 ${year.color}--text`
+                    "
+                  >
+                    Lorem ipsum
+                  </h2>
+                  <div>
+                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
+                    scaevola imperdiet nec ut, sed euismod convenire principes
+                    at. Est et nobis iisque percipit, an vim zril disputando
+                    voluptatibus, vix an salutandi sententiae.
+                  </div>
+                </div>
+              </v-timeline-item>
+            </v-timeline>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -10,9 +45,30 @@
 
 <script>
 export default {
-  methods: {
-    goHome() {
-      this.$router.push({ path: '/' })
+  data() {
+    return {
+      years: [
+        {
+          color: 'cyan',
+          year: '1960'
+        },
+        {
+          color: 'green',
+          year: '1970'
+        },
+        {
+          color: 'pink',
+          year: '1980'
+        },
+        {
+          color: 'amber',
+          year: '1990'
+        },
+        {
+          color: 'orange',
+          year: '2000'
+        }
+      ]
     }
   }
 }
