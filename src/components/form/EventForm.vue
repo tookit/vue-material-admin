a7e2f9f1878016b55efd2435b428ad8063d189f6
michaelwang <wangqiangshen@gmail.com>
2020-09-26 17:54:28 +0800

    update calendar

--- src/components/form/EventForm.vue
+++ src/components/form/EventForm.vue
@@ -4,11 +4,9 @@
       <v-row>
         <v-col :cols="12">
           <v-text-field
-            label="Title"
-            placeholder="Title"
-            name="title"
-            v-model="title"
-            dense
+            :label="form.title.lable"
+            :placeholder="form.title.placeholder"
+            v-model="formModel.title"
           />
         </v-col>
         <v-col :cols="6">
@@ -16,29 +14,31 @@
             ref="startDate"
             :close-on-content-click="false"
             v-model="startDateMenu"
+            :day-format="dateFormat"
             transition="scale-transition"
             offset-y
             max-width="290px"
-            :return-value.sync="startDate"
+            :return-value.sync="formModel.startDate"
           >
             <template v-slot:activator="{ on }">
               <v-text-field
-                label="Start Date"
-                v-model="startDate"
-                append-icon="mdi-calendar"
-                dense
+                :label="form.startDate.label"
+                :placeholder="form.startDate.placeholder"
+                v-model="formModel.startDate"
+                append-icon="mdi-clock-outline"
+                autocomplete="off"
                 v-on="on"
               />
             </template>
-            <v-date-picker v-model="startDate" no-title scrollable>
-              <v-spacer></v-spacer>
+            <v-date-picker v-model="formModel.startDate" no-title scrollable>
+              <v-spacer />
               <v-btn text color="primary" @click="startDateMenu = false"
                 >Cancel</v-btn
               >
               <v-btn
                 text
                 color="primary"
-                @click="$refs.startDate.save(startDate)"
+                @click="$refs.startDate.save(formModel.startDate)"
                 >OK</v-btn
               >
             </v-date-picker>
@@ -52,26 +52,26 @@
             transition="scale-transition"
             offset-y
             max-width="290px"
-            :return-value.sync="startTime"
+            :return-value.sync="formModel.startTime"
           >
             <template v-slot:activator="{ on }">
               <v-text-field
-                label="Start Time"
-                v-model="startTime"
+                :label="form.startTime.label"
+                :placeholder="form.startTime.placeholder"
+                v-model="formModel.startTime"
                 append-icon="mdi-clock-outline"
-                dense
                 v-on="on"
               />
             </template>
-            <v-time-picker v-model="startTime">
-              <v-spacer></v-spacer>
+            <v-time-picker v-model="formModel.startTime" format="24hr">
+              <v-spacer />
               <v-btn text color="primary" @click="startTimeMenu = false"
                 >Cancel</v-btn
               >
               <v-btn
                 text
                 color="primary"
-                @click="$refs.startTime.save(startTime)"
+                @click="$refs.startTime.save(formModel.startTime)"
                 >OK</v-btn
               >
             </v-time-picker>
@@ -85,23 +85,26 @@
             transition="scale-transition"
             offset-y
             min-width="290px"
-            :return-value.sync="endDate"
+            :return-value.sync="formModel.endDate"
           >
             <template v-slot:activator="{ on }">
               <v-text-field
-                label="End Date"
-                v-model="endDate"
+                :label="form.endDate.label"
+                :placeholder="form.endDate.placeholder"
+                v-model="formModel.endDate"
                 append-icon="mdi-calendar"
-                dense
                 v-on="on"
               />
             </template>
-            <v-date-picker v-model="endDate" no-title scrollable>
+            <v-date-picker v-model="formModel.endDate" no-title scrollable>
               <v-spacer />
               <v-btn text color="primary" @click="endDateMenu = false"
                 >Cancel</v-btn
               >
-              <v-btn text color="primary" @click="$refs.endDate.save(endDate)"
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.endDate.save(formModel.endDate)"
                 >OK</v-btn
               >
             </v-date-picker>
@@ -110,38 +113,44 @@
         <v-col :cols="6">
           <v-menu
             ref="endTime"
-            lazy
             :close-on-content-click="false"
             v-model="endTimeMenu"
             transition="scale-transition"
             offset-y
-            full-width
-            :nudge-bottom="-24"
             max-width="290px"
-            :return-value.sync="endTime"
+            :return-value.sync="formModel.endTime"
           >
             <template v-slot:activator="{ on }">
               <v-text-field
-                label="End Time"
-                v-model="endTime"
+                :label="form.endTime.lable"
+                :placeholder="form.endTime.placeholder"
+                v-model="formModel.endTime"
                 append-icon="mdi-clock-outline"
-                dense
                 v-on="on"
               />
             </template>
-            <v-time-picker v-model="endTime">
-              <v-spacer></v-spacer>
+            <v-time-picker v-model="formModel.endTime" format="24hr">
+              <v-spacer />
               <v-btn text color="primary" @click="endTimeMenu = false"
                 >Cancel</v-btn
               >
-              <v-btn text color="primary" @click="$refs.endTime.save(endTime)"
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.endTime.save(formModel.endTime)"
                 >OK</v-btn
               >
             </v-time-picker>
           </v-menu>
         </v-col>
         <v-col :cols="12">
-          <v-textarea outlined label="Description"> </v-textarea>
+          <v-textarea
+            outlined
+            :rows="2"
+            :label="form.remark.lable"
+            :placeholder="form.remark.placeholder"
+            v-model="formModel.remark"
+          />
         </v-col>
         <v-spacer />
       </v-row>
@@ -150,22 +159,77 @@
 </template>
 
 <script>
+import { format } from 'date-fns'
+const DATE_FORMAT = 'yyyy-MM-dd'
 export default {
-  data: () => ({
-    title: null,
-    valid: true,
-    startDateMenu: false,
-    startDate: null,
-    startTimeMenu: false,
-    startTime: null,
-    endDateMenu: false,
-    endDate: null,
-    endTimeMenu: false,
-    endTime: null
-  }),
+  props: {
+    event: Object
+  },
+  data() {
+    return {
+      valid: true,
+      dateFormat: DATE_FORMAT,
+      startDateMenu: false,
+      startTimeMenu: false,
+      endDateMenu: false,
+      endTimeMenu: false,
+      formModel: {
+        title: null,
+        startDate: null,
+        startTime: null,
+        endDate: null,
+        endTime: null,
+        remark: null
+      },
+      form: {
+        title: {
+          label: 'Title',
+          placeholder: 'Event Title',
+          rules: [(v) => !!v || 'This field is required']
+        },
+        startDate: {
+          label: 'Start date',
+          placeholder: 'Start date',
+          rules: [(v) => !!v || 'This field is required']
+        },
+        endDate: {
+          label: 'End date',
+          placeholder: 'End date',
+          rules: [(v) => !!v || 'This field is required']
+        },
+        startTime: {
+          label: 'Start time',
+          placeholder: 'Start time',
+          rules: [(v) => !!v || 'This field is required']
+        },
+        endTime: {
+          label: 'End time',
+          placeholder: 'end time',
+          rules: [(v) => !!v || 'This field is required']
+        },
+        remark: {
+          label: 'Remark',
+          placeholder: 'Remark',
+          rules: [(v) => !!v || 'This field is required']
+        }
+      }
+    }
+  },
+  watch: {
+    event: {
+      handler(event) {
+        this.updateEvent(event)
+      },
+      immediate: true
+    }
+  },
   methods: {
-    closeDialog() {
-      this.$parent.isActive = false
+    updateEvent({ name, color, start, end, timed }) {
+      this.formModel.title = name
+      this.formModel.startDate = format(start, DATE_FORMAT)
+      this.formModel.endDate = format(end, DATE_FORMAT)
+      this.formModel.startTime = format(start, 'HH:mm')
+      this.formModel.endTime = format(end, 'HH:mm')
     }
   }
 }
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,137 +1,186 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col :cols="12">
-        <v-sheet tile height="54" color="grey lighten-4" class="d-flex">
-          <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-select
-            v-model="type"
-            :items="types"
-            dense
-            outlined
-            hide-details
-            class="ma-2"
-            label="type"
-          />
-          <v-select
-            v-model="mode"
-            :items="modes"
-            dense
-            outlined
-            hide-details
-            label="event-overlap-mode"
-            class="ma-2"
-          />
-          <v-select
-            v-model="weekday"
-            :items="weekdays"
-            dense
-            outlined
-            hide-details
-            label="weekdays"
-            class="ma-2"
-          />
-          <v-spacer></v-spacer>
-          <v-btn icon class="ma-2" @click="$refs.calendar.next()">
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-btn>
+    <v-row class="fill-height">
+      <v-col>
+        <v-sheet height="64">
+          <v-toolbar flat dark color="primary">
+            <v-btn outlined class="mr-4" @click="setToday">
+              Today
+            </v-btn>
+            <v-btn fab text small @click="prev">
+              <v-icon small>mdi-chevron-left</v-icon>
+            </v-btn>
+            <v-btn fab text small @click="next">
+              <v-icon small>mdi-chevron-right</v-icon>
+            </v-btn>
+            <v-toolbar-title v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-menu bottom right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn outlined v-bind="attrs" v-on="on">
+                  <span>{{ typeToLabel[type] }}</span>
+                  <v-icon right>mdi-menu-down</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="type = 'day'">
+                  <v-list-item-title>Day</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'week'">
+                  <v-list-item-title>Week</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'month'">
+                  <v-list-item-title>Month</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = '4day'">
+                  <v-list-item-title>4 days</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
         </v-sheet>
         <v-sheet height="600">
           <v-calendar
             ref="calendar"
-            v-model="value"
-            :weekdays="weekday"
-            :type="type"
+            v-model="focus"
+            color="primary"
             :events="events"
-            :event-overlap-mode="mode"
-            :event-overlap-threshold="30"
             :event-color="getEventColor"
-            @change="getEvents"
-            @click:date="handleDateClick"
+            :type="type"
+            @click:event="showEvent"
+            @click:more="viewDay"
+            @click:date="viewDay"
             @click:day="handleDayClick"
             @click:time="handleTimeClick"
+            @change="updateRange"
           />
+          <v-menu
+            v-model="selectedOpen"
+            :close-on-content-click="false"
+            :activator="selectedElement"
+            offset-x
+          >
+            <v-card tile max-width="500px">
+              <v-toolbar flat :color="selectedEvent.color" dark>
+                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn @click="selectedOpen = false" icon>
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-card-text>
+                <event-form :event="selectedEvent" />
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="secondary" @click="selectedOpen = false">
+                  Cancel
+                </v-btn>
+                <v-spacer />
+                <v-btn tile color="primary" @click="selectedOpen = false">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
         </v-sheet>
       </v-col>
     </v-row>
-    <v-dialog v-model="showEventForm" max-width="640px">
-      <v-card>
-        <v-toolbar color="primary" dark>
-          <v-toolbar-title class="body-2">Add New Event</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="showEventForm = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-card-text>
-          <event-form />
-        </v-card-text>
-        <v-divider />
-        <v-card-actions>
-          <v-spacer />
-          <v-btn tile color="primary">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-container>
 </template>
 
 <script>
 import EventForm from '@/components/form/EventForm'
 export default {
+  name: 'PageCalendar',
   components: {
     EventForm
   },
-  data: () => ({
-    showEventForm: false,
-    type: 'week',
-    types: ['month', 'week', 'day', '4day'],
-    mode: 'stack',
-    modes: ['stack', 'column'],
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-      { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-      { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { text: 'Mon, Wed, Fri', value: [1, 3, 5] }
-    ],
-    value: '',
-    events: [],
-    colors: [
-      'blue',
-      'indigo',
-      'deep-purple',
-      'cyan',
-      'green',
-      'orange',
-      'grey darken-1'
-    ],
-    names: [
-      'Meeting',
-      'Holiday',
-      'PTO',
-      'Travel',
-      'Event',
-      'Birthday',
-      'Conference',
-      'Party'
-    ]
-  }),
+  data() {
+    return {
+      showEventForm: false,
+      focus: '',
+      type: 'month',
+      typeToLabel: {
+        month: 'Month',
+        week: 'Week',
+        day: 'Day',
+        '4day': '4 Days'
+      },
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      events: [],
+      colors: [
+        'blue',
+        'indigo',
+        'deep-purple',
+        'cyan',
+        'green',
+        'orange',
+        'grey darken-1'
+      ],
+      names: [
+        'Meeting',
+        'Holiday',
+        'PTO',
+        'Travel',
+        'Event',
+        'Birthday',
+        'Conference',
+        'Party'
+      ]
+    }
+  },
   methods: {
-    getEvents({ start, end }) {
+    viewDay({ date }) {
+      this.focus = date
+      this.type = 'day'
+    },
+    getEventColor(event) {
+      return event.color
+    },
+    setToday() {
+      this.focus = ''
+    },
+    prev() {
+      this.$refs.calendar.prev()
+    },
+    next() {
+      this.$refs.calendar.next()
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event
+        this.selectedElement = nativeEvent.target
+        setTimeout(() => (this.selectedOpen = true), 10)
+      }
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false
+        setTimeout(open, 10)
+      } else {
+        open()
+      }
+
+      nativeEvent.stopPropagation()
+    },
+    updateRange({ start, end }) {
       const events = []
+
       const min = new Date(`${start.date}T00:00:00`)
       const max = new Date(`${end.date}T23:59:59`)
       const days = (max.getTime() - min.getTime()) / 86400000
       const eventCount = this.rnd(days, days + 20)
+
       for (let i = 0; i < eventCount; i++) {
         const allDay = this.rnd(0, 3) === 0
         const firstTimestamp = this.rnd(min.getTime(), max.getTime())
         const first = new Date(firstTimestamp - (firstTimestamp % 900000))
         const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
         const second = new Date(first.getTime() + secondTimestamp)
+
         events.push({
           name: this.names[this.rnd(0, this.names.length - 1)],
           start: first,
@@ -143,9 +192,6 @@ export default {
 
       this.events = events
     },
-    getEventColor(event) {
-      return event.color
-    },
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a
     },
@@ -153,11 +199,11 @@ export default {
       console.log(e)
     },
     handleDayClick(e) {
-      console.log(e)
       this.showEventForm = true
     },
     handleTimeClick(e) {
       console.log(e)
+      // this.selectedEvent = null
     }
   }
 }
