53625920932ee17c4414538ad05b8cf503522c43
michaelwang <wangqiangshen@gmail.com>
2021-03-29 08:56:41 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'plugin:vue/recommended', '@vue/prettier'],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-unused-vars': 1,
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+}
